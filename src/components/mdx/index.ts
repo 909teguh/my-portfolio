324af37b679717340e66c35e07749d60121b9d29
909teguh <suwandateguh@gmail.com>
2026-03-17 08:04:13 +0700

    update tampilan dan komponen
    sekarang komponen sudah siap digunakan

---
+++ src/components/mdx/index.ts
@@ -0,0 +1,4 @@
+// src/components/mdx/index.ts
+// Re-export semua komponen MDX dari satu titik
+export { default as Callout } from './Callout.astro';
+export { default as ImageFloat } from './ImageFloat.astro';
